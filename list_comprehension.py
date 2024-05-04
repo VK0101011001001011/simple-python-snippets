393e3edbc5b7d7993a20f642c058f185a4b8ae79
Vell <137341548+VellVoid@users.noreply.github.com>
2024-05-04 08:29:09 +0200

    Update list_comprehension.py
    Comments added

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,3 +1,5 @@
-numbers = [x for x in range(1, 11) if x % 2 == 0]
+# Create a list of even numbers from 1 to 10 
+numbers = [x for x in range(1, 11) if x % 2 == 0]  # List comprehension for filtering even numbers
 
-print("Even numbers:", numbers)
+# Print the list of even numbers
+print("Even numbers:", numbers) 
