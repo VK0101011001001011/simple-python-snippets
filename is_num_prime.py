f25409acb16c2b9b1d3adef69daa2267fc661d9b
Vell <137341548+VellVoid@users.noreply.github.com>
2024-05-04 08:28:32 +0200

    Update is_num_prime.py
    Comments added

--- is_num_prime.py
+++ is_num_prime.py
@@ -1,11 +1,23 @@
-num = 17
+# Prime number checker
 
+num = 17  # The number to check for primality
+
+# Check if the number is greater than 1 (definition of prime numbers)
 if num > 1:
-    for i in range(2, int(num/2) + 1):
-        if (num % i) == 0:
-            print(num, "is not prime")
-            break
-    else:
-        print(num, "is prime")
+
+  # Iterate from 2 up to half the number (optimization for prime checking)
+  for i in range(2, int(num/2) + 1):
+
+    # Check for divisibility by any number in the loop
+    if (num % i) == 0: 
+      print(num, "is not prime")  # If divisible, it's not prime
+      break  # Exit the loop, no need to check further
+
+  # If the loop completes without finding divisors, the number is prime
+  else:
+    print(num, "is prime") 
+
+# Numbers less than or equal to 1 are not prime
 else:
-    print(num, "is not prime")
+  print(num, "is not prime") 
+
