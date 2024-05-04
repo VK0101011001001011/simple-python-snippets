5530b1cabf208696f51f42457b36648c1c8c56f0
Vell <137341548+VellVoid@users.noreply.github.com>
2024-05-04 08:26:48 +0200

    Update factorial.py
    Comments added

--- factorial.py
+++ factorial.py
@@ -1,7 +1,13 @@
-num = 5
-factorial = 1
+# This program calculates the factorial of a number
 
+num = 5  # The number to find the factorial of
+
+factorial = 1  # Initialize the factorial with 1 (important for the calculation)
+
+# Iterate from 1 to the number (inclusive)
 for i in range(1, num + 1):
-    factorial *= i
+  # Update the factorial by multiplying it with the current loop value 
+  factorial *= i
 
+# Print the calculated factorial 
 print("Factorial of", num, "is", factorial)
